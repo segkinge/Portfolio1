7cea754e24fad36a7d3f968990f13ff5cf574786
segkinge <101500654+segkinge@users.noreply.github.com>
2022-07-30 00:10:42 +0100

    First portfolio website

---
+++ JavaScript.js
@@ -0,0 +1,14 @@
+function toggle(){
+let a =document.getElementById('first')
+let b = document.getElementById('left')
+{
+if (b.style.display = 'none') {
+    b.style.display = 'block'
+} else {
+    b.style.display = 'none'
+}
+}
+}
+function Message(){
+    alert('Message sent successfully')
+}
